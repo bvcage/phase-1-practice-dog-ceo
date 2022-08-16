5dfea2e436e41b827f3f7974169b7e3348031e23
Bailey Cage <bailey.cage@gmail.com>
2022-08-16 16:10:17 -0400

    fetch and display dog photos

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 console.log('%c HI', 'color: firebrick')
+
+const IMG_URL = "https://dog.ceo/api/breeds/image/random/4";
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    // dog image container
+    const dogImages = document.querySelector('#dog-image-container');
+
+    // get dog images
+    fetch(IMG_URL)
+    .then((response) => response.json())
+    .then((data) => {
+        const dogPicSrcs = data.message;
+        dogPicSrcs.forEach(imgSrc => {
+            const newImg = document.createElement('img');
+            newImg.src = imgSrc;
+            dogImages.append(newImg);
+        });
+    })
+    .catch((error) => console.log(error));
+
+
+});
